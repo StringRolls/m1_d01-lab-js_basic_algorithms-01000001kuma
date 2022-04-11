c3911b75d253b69a6cf65dfbe0b8ef1da5151617
01000001kuma <a_illera@yahoo.com>
2022-04-11 19:46:15 +0200

    Doing: iteration 3

--- js/index.js
+++ js/index.js
@@ -1,9 +1,32 @@
 // Iteration 1: Names and Input
 
+let hacker1 = "Peter";
+
+console.log(`The driver's name is ${hacker1}`)
+
+let hacker2 = "Jane";
+
+console.log(`The driver's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 
+if (hacker1.length < hacker2.length){
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters`);
+} else if (hacker2.length < hacker1.length){
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
+} else {
+    console.log(`Wow, you both have equally long names, ${hacker1.length}+${hacker2.length} characters!`);
+}
+
 // Iteration 3: Loops
+
+for (let i = 0; i < hacker1.length; i++) {
+    console.log(hacker1.toLocaleUpperCase);
+    
+}
+
+
+
 // Iteration 3.1 'Name' to 'N A M E'
 
 // Iteration 3.2 'Name' to 'emaN'
